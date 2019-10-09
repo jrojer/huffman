26d556e9d44aea39eca38b89fe8e1ac084146db0
jrojer <cibid@yandex.ru>
2019-10-09 15:04:47 +0300

    реализация алгоритма кодирования

--- sources/huffman.h
+++ sources/huffman.h
@@ -5,6 +5,8 @@
 
 namespace huffman
 {
+const size_t kByteLen      = 8;
+const size_t kAlphabetSize = 256;
 struct HistItem
 {
     uint8_t value;
@@ -18,15 +20,13 @@ bool operator<(const HistItem& lhs, const HistItem& rhs)
 
 std::vector<HistItem> MakeHistogram(const std::vector<uint8_t>& items)
 {
-    const int alphabet_size = 256;
-
-    std::array<size_t, alphabet_size> hist = {};
+    std::array<size_t, kAlphabetSize> hist = {};
     for (auto x : items)
     {
         ++hist[x];
     }
     std::vector<HistItem> res;
-    for (int i = 0; i < alphabet_size; ++i)
+    for (int i = 0; i < kAlphabetSize; ++i)
     {
         if (hist[i] > 0)
             res.push_back({ static_cast<uint8_t>(i), hist[i] });
@@ -48,7 +48,7 @@ Node* MakeTree(const std::vector<HistItem>& hist)
     std::vector<Node*> heap;
     for (auto x : hist)
     {
-        heap.push_back(new Node{ x.value, x.weight, nullptr, nullptr,false });
+        heap.push_back(new Node{ x.value, x.weight, nullptr, nullptr, false });
     }
     const auto cmp = [](Node* a, Node* b) { return a->weight > b->weight; };
     std::make_heap(heap.begin(), heap.end(), cmp);
@@ -58,31 +58,31 @@ Node* MakeTree(const std::vector<HistItem>& hist)
         std::pop_heap(heap.begin(), heap.end(), cmp);
         Node* n2 = heap.front();
         std::pop_heap(heap.begin(), heap.end(), cmp);
-        heap.push_back(new Node{ 0, n1->weight + n2->weight, n1, n2,false });
+        heap.push_back(new Node{ 0, n1->weight + n2->weight, n1, n2, false });
         std::push_heap(heap.begin(), heap.end(), cmp);
     }
     Node* root = heap.front();
     return root;
 }
 
-std::array<std::vector<int>, 256> GetCodeTable(Node* root)
+std::array<std::vector<int>, kAlphabetSize> GetCodeTable(Node* root)
 {
-    std::array<std::vector<int>, 256> res = {};
-    std::vector<Node*>          stack(1, root);
-    std::vector<int>            code;
+    std::array<std::vector<int>, kAlphabetSize> res = {};
+    std::vector<Node*>                          stack(1, root);
+    std::vector<int>                            code;
     while (!stack.empty())
     {
-        Node* node = stack.back();
+        Node* node    = stack.back();
         node->visited = true;
         if (node->left && node->right)
         {
-            if(!node->left->visited)
+            if (!node->left->visited)
             {
                 stack.push_back(node);
                 stack.push_back(node->left);
                 code.push_back(0);
             }
-            else if(!node->right->visited)
+            else if (!node->right->visited)
             {
                 stack.push_back(node);
                 stack.push_back(node->right);
@@ -91,7 +91,7 @@ std::array<std::vector<int>, 256> GetCodeTable(Node* root)
             else
             {
                 stack.pop_back();
-                if(!code.empty())
+                if (!code.empty())
                     code.pop_back();
             }
         }
@@ -99,7 +99,7 @@ std::array<std::vector<int>, 256> GetCodeTable(Node* root)
         {
             res[node->value] = code;
             stack.pop_back();
-            if(!code.empty())
+            if (!code.empty())
                 code.pop_back();
         }
     }
@@ -113,7 +113,7 @@ void DeleteTree(Node* root)
     {
         Node* node = stack.back();
         stack.pop_back();
-        if(node != nullptr)
+        if (node != nullptr)
         {
             stack.push_back(node->left);
             stack.push_back(node->right);
@@ -122,16 +122,63 @@ void DeleteTree(Node* root)
     }
 }
 
+void SetBit(uint8_t* dst, size_t pos, int bit)
+{
+    size_t byte_pos = pos / kByteLen;
+    size_t bit_pos  = pos % kByteLen;
+    dst[byte_pos] &= ~(1 << bit_pos);
+    dst[byte_pos] ^= (bit << bit_pos);
+}
+
+void WriteSizeT(uint8_t* arr, size_t s)
+{
+    uint8_t* a = reinterpret_cast<uint8_t*>(&s);
+    for (size_t i = 0; i < sizeof(size_t); ++i)
+    {
+        arr[i] = a[i];
+    }
+}
+
+void WriteHistogram(uint8_t* dst, const std::vector<HistItem>& hist)
+{
+    for (auto x : hist)
+    {
+        *dst = x.value;
+        ++dst;
+        WriteSizeT(dst, x.weight);
+        dst += sizeof(size_t);
+    }
+}
+
 std::vector<uint8_t> Encode(const std::vector<uint8_t>& chunk)
 {
     const auto hist       = MakeHistogram(chunk);
     const auto tree       = MakeTree(hist);
     const auto code_table = GetCodeTable(tree);
-    // process chunk with code table
-    // ...
+
+    const size_t         histogram_size = (sizeof(uint8_t) + sizeof(size_t)) * kAlphabetSize;
+    const size_t         meta_data_size = histogram_size + sizeof(size_t);
+    std::vector<uint8_t> result(meta_data_size + chunk.size());
+
+    WriteHistogram(result.data(), hist);
+
+    size_t result_bitlen = 0;
+
+    uint8_t* dst = result.data() + meta_data_size;
+    for (uint8_t x : chunk)
+    {
+        for (int bit : code_table[x])
+        {
+            SetBit(dst, result_bitlen, bit);
+            ++result_bitlen;
+        }
+    }
     DeleteTree(tree);
-    return {};
+    result.resize(meta_data_size + (result_bitlen + kByteLen - 1) / kByteLen);
+    WriteSizeT(result.data() + histogram_size, result_bitlen);
+    return result;
 }
+
 std::vector<uint8_t> Decode(const std::vector<uint8_t>& chunk)
 {
     // for
