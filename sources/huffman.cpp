74fa3bf7f3649d17ee6546fdfdb5a6a77e20b2c6
Alexis Rousanov <cibid@yandex.ru>
2019-11-18 13:22:23 +0300

    Update huffman.cpp

--- sources/huffman.cpp
+++ sources/huffman.cpp
@@ -275,7 +275,6 @@ void PrintCodeTable(const std::array<std::vector<int>, kAlphabetSize>& code_tabl
     {
         out << item.code << " " << item.value << std::endl;
     }
-    DeleteTree(tree);
 }
 
 }  // namespace huffman
